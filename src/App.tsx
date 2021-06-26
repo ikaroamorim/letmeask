73919edbada23de7d9a96919f7e5955381191df7
ikaroamorim <ikaro.amorim@gmail.com>
2021-06-26 18:22:01 -0300

    Explicação Estados

--- src/App.tsx
+++ src/App.tsx
@@ -5,11 +5,7 @@ import { Button } from './components/Button';
 function App() {
   return (
     <div>
-      <Button text="teste123" />
-      <Button text="teste123" />
       <Button />
-      <Button text="teste123" >ABC 123</Button>
-      <Button text="teste123" />
     </div>
   );
 }
--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,11 +1,25 @@
+import { useState } from "react";
+
 type ButtonProps = {
    children?: string;
    text?: string;
 }
 
 export function Button(props: ButtonProps) {
+   //let counter = 0;
+   const [counter, setCounter] = useState(0);
+
+   function increment() {
+      //counter += 1;
+      setCounter(counter + 1);
+      console.log(counter);
+   }
+
+
    return (
-      <button> {props.text || 'Default'} </button>
+      <button onClick={increment}>
+         {counter}
+      </button>
    )
 }
 
